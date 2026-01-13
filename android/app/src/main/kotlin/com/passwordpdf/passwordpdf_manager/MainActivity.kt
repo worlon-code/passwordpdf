6aa8b0c2247b502c15cd05120bcf7c6e8be6ad24
worlon-code <sai2092000@gmail.com>
2026-01-14 04:05:47 +0530

    removed rename screenshot code

--- android/app/src/main/kotlin/com/passwordpdf/passwordpdf_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/passwordpdf/passwordpdf_manager/MainActivity.kt
@@ -4,20 +4,7 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import android.os.Bundle
 
 class MainActivity: FlutterFragmentActivity() {
-    private var screenshotHandler: ScreenshotRenameHandler? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        screenshotHandler = ScreenshotRenameHandler(this)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        screenshotHandler?.register()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        screenshotHandler?.unregister()
     }
 }
